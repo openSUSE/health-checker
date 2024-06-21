8b6849300b20db130ad7cc610dc58d694a58cc3d
Frederic Crozat <fcrozat@suse.com>
2024-06-21 16:44:25 +0200

    Add rpm db consistency plugin
    Ensure RPM database is always consistent (all dependencies are
    fulfilled) and revert to previous snapshot if not.

---
+++ plugins/rpmdb-consistency.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+run_checks() {
+	zypper --no-refresh --quiet verify --dry-run
+	test $? -ne 0 && exit 1
+}
+
+case "$1" in
+    check)
+	run_checks
+	;;
+    *)
+	echo "Usage: $0 {check|stop}"
+	exit 1
+	;;
+esac
+
+exit 0
