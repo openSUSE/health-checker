fd4f1f761b3c5ebb32f36fc8d915b36de422ce9d
Frederic Crozat <fcrozat@suse.com>
2024-09-25 11:40:18 +0200

    remove zypper verify call
    Will create a separate plugin for zypper verify

--- plugins/rpmdb-consistency.sh
+++ plugins/rpmdb-consistency.sh
@@ -3,9 +3,6 @@
 run_checks() {
 	# check rpm DB itself (need to override lock path, the default one is read-only)
 	rpm -D "%_rpmlock_path /run/rpmdb" --verifydb || exit 1
-	# only rely on local/system repository for check
-	zypper --no-refresh --no-remote --non-interactive --quiet verify --dry-run
-	test $? -ne 0 && exit 1
 }
 
 case "$1" in
