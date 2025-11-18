12fb7cfceb2a872a1bb319a5fd5816da875d67fd
Danilo Spinella <danilo.spinella@suse.com>
2025-11-18 14:10:02 +0100

    fix(dracut): Skip dracut module on BLS systems

--- dracut/module-setup.sh
+++ dracut/module-setup.sh
@@ -2,18 +2,24 @@
 
 # called by dracut
 check() {
-    require_binaries logger date || return 1
+    if [ ! -x /bin/sdbootutil ]; then
+        require_binaries logger date || return 1
+    fi
 }
 
 # called by dracut
 depends() {
-    echo drm
+    if [ ! -x /bin/sdbootutil ]; then
+        echo drm
+    fi
 }
 
 # called by dracut
 install() {
-    inst_hook emergency 90 "$moddir"/health-checker-emergency.sh
+    if [ ! -x /bin/sdbootutil ]; then
+        inst_hook emergency 90 "$moddir"/health-checker-emergency.sh
 
-    inst_multiple date btrfs awk grub2-editenv
+        inst_multiple date btrfs awk grub2-editenv
+    fi
 }
 
